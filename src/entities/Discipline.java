f16c94af047c728de2a14581f9b73601a01a0b36
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-21 19:52:12 -0300

    retirando comments desnecessarios

--- src/entities/Discipline.java
+++ src/entities/Discipline.java
@@ -4,19 +4,12 @@ import java.util.ArrayList;
 
 public class Discipline {
     private String name;
-   // private Integer NumberOfStudents = 0;
     private String data;
-   // private ArrayList<Student> estudantes = new ArrayList<>();
-
-    public Discipline() {
-
-    }
 
     public Discipline(String name, String data) {
         this.name = name;
         this.data = data;
     }
-
 
     public String getName() {
         return name;
